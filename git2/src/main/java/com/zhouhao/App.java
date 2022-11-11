0549efb54bae448c8168173222b10200e114de32
zhouhao <1648262943@qq.com>
2022-11-11 14:11:47 +0800

    hot commit1

--- git2/src/main/java/com/zhouhao/App.java
+++ git2/src/main/java/com/zhouhao/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("woani");
         System.out.println("master");
+        System.out.println("hot");
     }
 }
